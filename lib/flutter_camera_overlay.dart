58f80911cff0cab2a22a51424cf48316abe24615
Esmaeil Ahmadipour <software8686@gmail.com>
2022-06-28 11:25:02 -0700

    fixed dart sdk for old null-safety version .

--- lib/flutter_camera_overlay.dart
+++ lib/flutter_camera_overlay.dart
@@ -1,5 +1,4 @@
 import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_camera_overlay/model.dart';
